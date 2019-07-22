6f57293f7443a47b6eba3c1c37ebb3fda1d7823e
joestack <joern@joern-online.de>
2019-07-22 21:08:32 +0200

    3nodes

--- variables.tf
+++ variables.tf
@@ -3,6 +3,11 @@ variable "aks_prefix" {
   description = "A prefix used for all resources in this example"
 }
 
+variable "node_count" {
+  description = "amount of k8s minions"
+  default = "3"
+}
+
 
 variable "kubernetes_client_id" {
   description = "The Client ID for the Service Principal to use for this Managed Kubernetes Cluster"
